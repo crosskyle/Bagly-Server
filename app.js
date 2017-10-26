8fe0e078fe935ce4f66cc3a4272802823212d792
crosskyle <kylcross@gmail.com>
2017-10-26 16:06:43 -0700

    Second deploy try

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const routes = require('./routes/routes')
 const app = express()
 const cors = require('cors')
-/*
+
 mongoose.Promise = global.Promise
 
 if(process.env.NOD_ENV !== 'test') {
@@ -14,18 +14,12 @@ if(process.env.NOD_ENV !== 'test') {
 
 app.use(morgan('combined'))
 app.use(cors())
-*/
-//app.use(bodyParser.json({ type: '*/*' }))
-/*
+app.use(bodyParser.json({ type: '*/*' }))
+
 routes(app)
 
 app.use((err, req, res, next) => {
   res.status(422).send({ error: err._message })
-})
-*/
-
-app.get('/', (req, res) => {
-  res.send({hi: 'there'})
 })
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const app = require('./app')
+//const app = require('./app')
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' })
+})
 
 const PORT = process.env.PORT || 3050
 app.listen(PORT)
